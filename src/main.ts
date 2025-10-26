fdb3d244268b3ef7c915a1dee1238b5335d28a84
Alvaro Maxter <themaxter99@gmail.com>
2025-10-26 21:13:47 +0100

    refactor: remove @vercel/speed-insights import from main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,5 @@ import { App } from './app/app';
 
 //Vercel imports
 import { inject } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
