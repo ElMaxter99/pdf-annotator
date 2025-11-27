3081182a1044c72d5087076792ad5d2c2f389721
Alvaro Amorós <39102625+ElMaxter99@users.noreply.github.com>
2025-11-27 19:31:19 +0100

    Add light/dark theme toggle

--- src/app/pages/landing/components/language-bar/landing-language-bar.component.ts
+++ src/app/pages/landing/components/language-bar/landing-language-bar.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { LanguageSelectorComponent } from '../../../../components/language-selector/language-selector.component';
+import { ThemeToggleComponent } from '../../../../components/theme-toggle/theme-toggle.component';
 import { Language } from '../../../../i18n/translation.service';
 
 @Component({
   selector: 'app-landing-language-bar',
   standalone: true,
-  imports: [CommonModule, LanguageSelectorComponent],
+  imports: [CommonModule, LanguageSelectorComponent, ThemeToggleComponent],
   templateUrl: './landing-language-bar.component.html',
 })
 export class LandingLanguageBarComponent {
